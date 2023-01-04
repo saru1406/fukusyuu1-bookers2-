e4541713ee0b85650a847f69862a4fdd72485437
EC2 Default User <110827986+saru1406@users.noreply.github.com>
2023-01-04 01:53:08 +0000

    [add]header作成

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,21 @@
+class BooksController < ApplicationController
+  
+  def index
+  end
+  
+  def show
+  end
+  
+  def edit
+  end
+  
+  def create
+  end
+  
+  def update
+  end
+  
+  def destroy
+  end
+  
+end
